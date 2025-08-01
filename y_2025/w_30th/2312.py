4b6abc99517fb758db3e31484caba387ac7ba023
SeoYoonmi <sym2005sym@naver.com>
2025-08-01 17:14:03 +0900

    0801

--- y_2025/w_30th/2312.py
+++ y_2025/w_30th/2312.py
@@ -1,4 +1,16 @@
 n = int(input())
 for _ in range(n):
     num = int(input())
-    print(num)
+    p = 2
+    prime_dict = {}
+    while p <= num:
+        if num % p == 0:
+            num /= p 
+            if p in prime_dict:
+                prime_dict[p] += 1
+            else:
+                prime_dict[p] = 1
+        else:
+            p += 1
+    for x in prime_dict:
+        print(x, prime_dict[x])
